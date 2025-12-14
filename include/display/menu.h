dd93a5734c892c3b9896d333b844338c33d28d31
Iyashini <contact.jgo0@gmail.com>
2025-12-14 13:29:16 +0100

    Push 6

--- include/display/menu.h
+++ include/display/menu.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <Adafruit_SSD1306.h>
+#include "input/button.h" // Inclus pour l'événement de bouton
 
 // Constantes menu
 extern const int MENU_SIZE;
@@ -9,3 +10,7 @@ extern int menuIndex;
 
 // Affichage du menu principal
 void afficherMenu(Adafruit_SSD1306 &display);
+
+// Gère les entrées (encodeur rotation et clic) pour la navigation dans le menu.
+// *currentScreen est un pointeur vers la variable d'état de l'écran (dans main.cpp)
+void menu_handleInput(Adafruit_SSD1306 &display, int *currentScreen); // <-- CETTE LIGNE MANQUAIS
